cb8b5f94efe0b1b01d66aa46dd868eaefcfe52b4
Christina Huang <christinahuang574@gmail.com>
2025-08-12 11:11:46 -0700

    Initial Commit

---
+++ jymmy/jymmyApp.swift
@@ -0,0 +1,17 @@
+//
+//  jymmyApp.swift
+//  jymmy
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct jymmyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
