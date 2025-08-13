9a6fa038447eb09b670acbf12d7314f54c8495ae
Kylie Davis <kylie@jtde.com>
2025-08-13 11:19:59 -0700

    home color blocks

--- jymmy/home.swift
+++ jymmy/home.swift
@@ -10,41 +10,25 @@ import SwiftUI
 struct home: View {
     var body: some View {
         VStack{
-            Color("creamwhite")
-                .ignoresSafeArea()
-            
+        
             ZStack{
-                VStack{//vstack 2
-                    Text("Welcome Emily!")
-                    .foregroundColor(Color.white)          }//VStack 2 end
-                .background(Rectangle().foregroundColor(.mediumteal))
-                .cornerRadius(15)
-                .padding(20)//end2
-                
-                VStack{//vstack 3
-                    Text("Recent workouts")
-                        .foregroundColor(Color.white)
-                }//Vstack 3 end
-                .background(Rectangle().foregroundColor(.verylightteal))
-                .cornerRadius(15)
-                .padding()//end 3
-                
-                VStack{//vstack4
-                    Text("Exersices reccommended for you")
-                        .foregroundColor(Color.white)
-                }//vstack4
-                .background(Rectangle().foregroundColor(.darkteal))
-                .cornerRadius(15)
-                .padding(20)
-                
-                
-                
-                
+                VStack(spacing:30){//vstack 2
+                    
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.verylightteal)
+                        .frame(width:300, height:150, alignment:.topLeading)
+                    
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.darkteal)
+                        .frame(width:300, height:150, alignment:.center)
+                    
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.mediumteal)
+                        .frame(width:300, height:150,)
+                    
+                }//vstack2 end
             }//Zstack end
         }//Vstack 1 end
-        .background(Rectangle().foregroundColor(.red))
-        .cornerRadius(15)
-        .padding(20)
     }//body end
 }//struct end
 
