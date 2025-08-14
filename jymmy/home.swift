61c018738e8c1ba5b4aae551db1a81067d10a8f4
Sophie <yummylemon8@gmail.com>
2025-08-14 13:53:13 -0700

    Home page: formatting, word positioning, editing text

--- jymmy/home.swift
+++ jymmy/home.swift
@@ -15,47 +15,47 @@ struct home: View {
         
                   RoundedRectangle(cornerRadius: 15)
                     .fill(Color.verylightteal)
-                    .frame(width:300, height:150, alignment:.topLeading)
+                    .frame(width:380, height:150, alignment:.topLeading)
             
                   RoundedRectangle(cornerRadius: 15)
                      .fill(Color.darkteal)
-                      .frame(width:300, height:150, alignment:.center)
+                      .frame(width:380, height:150, alignment:.center)
                   
                     RoundedRectangle(cornerRadius: 15)
                        .fill(Color.mediumteal)
-                     .frame(width:300, height:150,)
+                     .frame(width:380, height:190,)
             }//vstack2 end
             
-            Text("Your only limit is you")
-                .font(.title)
+            Text("Your only limit is you.")
+                .font(.system(size: 34))
                 .fontWeight(.bold)
                 .foregroundColor(Color.darkteal)
-                .position(x:200, y:210)
+                .position(x:194, y:180)
 
-            Text("Everyday is a chance")
-                .font(.title2)
+            Text("Every day is a chance...")
+                .font(.system(size: 33))
                 .fontWeight(.bold)
                 .foregroundColor(Color.white)
-                .position(x:200, y:375)
+                .position(x:200, y:330)
             
-            Text("to get stronger")
-                .font(.title2)
+            Text("TO GET STRONGER.")
+               .font(.system(size: 34))
                 .fontWeight(.bold)
                 .foregroundColor(Color.white)
-                .position(x:200, y:410)
+                .position(x:200, y:390)
             
             
-            Text("Don't let fear hold you back,")
-                .font(.title2)
+            Text("Don't let fear hold you back!")
+                .font(.system(size: 28))
                 .fontWeight(.bold)
                 .foregroundColor(Color.white)
-                .position(x:200, y:550)
-            
-            Text(" take the first step")
-                .font(.title2)
+                .position(x:200, y:525)
+                
+            Text("Take the first step.")
+                .font(.system(size: 37))
                 .fontWeight(.bold)
                 .foregroundColor(Color.white)
-                .position(x:200, y:585)
+                .position(x:200, y:590)
         
             
         }//Zstack end
